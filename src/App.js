3dd331c2903cb94d485bbf4010bc8cf761095523
Natalia <lau.nat.her.her@gmail.com>
2021-11-29 18:52:16 -0600

    campaignsforms

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import React from "react";
 
-import { Donate } from "./views/Donate";
+// import { Donate } from "./views/Donate";
+import Campaigns3 from "./views/Campaigns/campaigns3";
 
 function App() {
   return (
     <div className="App">
-      <Donate />
+      <Campaigns3></Campaigns3>
     </div>
   );
 }
