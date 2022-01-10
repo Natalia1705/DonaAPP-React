cd53bb518c47c4c282d7ab245179ee3aad719f3c
torvic <130111@unsaac.edu.pe>
2022-01-10 02:07:28 -0500

    Create store & add it in App.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux';
+import store from './state/store';
 import reportWebVitals from './reportWebVitals';
 import './styles/index.scss';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
