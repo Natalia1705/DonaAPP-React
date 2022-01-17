24b1acd727b59a8fe79dc3d0d726ad6878c69146
torvic <130111@unsaac.edu.pe>
2022-01-17 10:05:15 -0500

    Add AsyncThunk postSignIn to sign in with the api

--- src/api/auth/authRequest.js
+++ src/api/auth/authRequest.js
@@ -1,7 +1,14 @@
 const URL_BASE = 'http://localhost:5000/api';
 
-export const signIn = () => {
-  const query = fetch(`${URL_BASE}/user/signin`).then((res) => res.json());
+export const signIn = (user) => {
+  const query = fetch(`${URL_BASE}/user/signin`, {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  });
   return query;
 };
 
--- src/thunkAction/authThunk.js
+++ src/thunkAction/authThunk.js
@@ -2,7 +2,10 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { signIn, signUp } from '../api/auth/authRequest';
 
 // prettier-ignore
-export const postSignIn = createAsyncThunk('auth/postSignIn', async () => signIn());
+export const postSignIn = createAsyncThunk('auth/postSignIn', async (user) => {
+  const res = await signIn(user);
+  return res.json();
+});
 
 // prettier-ignore
 export const postSignUp = createAsyncThunk('auth/postSignUp', async (user) => {
