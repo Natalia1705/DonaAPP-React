ac48c15d367d39a26cff57f0a6b34d809ac78d88
Natalia <lau.nat.her.her@gmail.com>
2022-01-22 13:27:23 -0600

    form actualizations

--- src/views/CampaignForm/index.jsx
+++ src/views/CampaignForm/index.jsx
@@ -1,16 +1,15 @@
-/* eslint-disable eqeqeq */
-/* eslint-disable no-console */
-/* eslint-disable no-alert */
 /* eslint-disable react/button-has-type */
-import React, { useState } from 'react';
-import Step1 from './components/Step1';
-import Step2 from './components/Step2';
-import Step3 from './components/Step3';
+/* eslint-disable no-console */
+/* eslint-disable react/function-component-definition */
+/* eslint-disable no-undef */
+/* eslint-disable arrow-body-style */
+import { useState } from 'react';
 import './style.scss';
+// import { Formik, Form, Field, ErrorMessage } from 'formik';
+// import * as Yup from 'yup';
 
-const CampaignForm = () => {
-  const [page, setPage] = useState(0);
-  const [formData, setFormData] = useState({
+export default function App() {
+  const [data, setData] = useState({
     name: '',
     country: '',
     category: '',
@@ -19,64 +18,166 @@ const CampaignForm = () => {
     title: '',
     img: '',
   });
+  const [currentStep, setCurrentStep] = useState(0);
 
-  const FormTitles = [
-    'Inicia tu campaña',
-    ' Establece el objetivo de tu campaña',
-    'Cuenta tu historia',
-  ];
-
-  // eslint-disable-next-line react/no-unstable-nested-components
-  const PageDisplay = () => {
-    if (page === 0) {
-      return <Step1 formData={formData} setFormData={setFormData} />;
-    }
-    if (page === 1) {
-      return <Step2 formData={formData} setFormData={setFormData} />;
-    }
-    return <Step3 formData={formData} setFormData={setFormData} />;
+  const makeRequest = (formData) => {
+    console.log('Form Submitted', formData);
   };
 
+  const handleNextStep = (newData, final = false) => {
+    setData((prev) => ({ ...prev, ...newData }));
+
+    if (final) {
+      makeRequest(newData);
+      return;
+    }
+
+    setCurrentStep((prev) => prev + 1);
+  };
+
+  const handlePrevStep = (newData) => {
+    setData((prev) => ({ ...prev, ...newData }));
+    setCurrentStep((prev) => prev - 1);
+  };
+
+  const steps = [
+    <Step1 next={handleNextStep} data={data} />,
+    <Step2 next={handleNextStep} prev={handlePrevStep} data={data} />,
+    <Step3 next={handleNextStep} prev={handlePrevStep} data={data} />,
+  ];
+
+  console.log('data', data);
+
+  return <div className="App">{steps[currentStep]}</div>;
+}
+
+// const step1ValidationSchema = Yup.object({
+//   first_name: Yup.string().required().label('First name'),
+//   last_name: Yup.string().required().label('Last name'),
+// });
+
+const Step1 = () => {
+  // const handleSubmit = (values) => {
+  //   props.next(values);
+  // };
   return (
     <div className="root">
-      <div className="form-register">
+      <form action="" className="form-register">
         <div className="form-register__header">
           <ul className="progressbar">
+            <li className="progressbar__option active" />
             <li className="progressbar__option" />
-            <li className="progressbar__option " />
             <li className="progressbar__option" />
           </ul>
-          <h1>{FormTitles[page]}</h1>
+          <h1 className="form-register__title">Inicia tu campaña</h1>
         </div>
-        <div className="body">{PageDisplay()}</div>
-        <div className="step__footer">
-          <button
-            className="step__button step__button--next"
-            disabled={page === 0}
-            onClick={() => {
-              setPage((currPage) => currPage - 1);
-            }}
-          >
-            Regresar
-          </button>
+        <div className="form-register__body">
+          <div className="step active" id="step-1">
+            <div className="step__body">
+              <p>Nombre</p>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Tu nombre ej:Juan Pérez"
+                className="step__input"
+              />
+              <p>¿Dónde vives?</p>
+              <input />
+              <p>¿Para que estás recaudando fondos?</p>
+              <select>
+                <option value="health">Salud</option>
+                <option value="emergency">Emergency</option>
+                <option value="funerals">In memoriam</option>
+                <option value="animals">Animales</option>
+              </select>
+            </div>
+            <div className="step__footer">
+              <button className="step__button step__button--next">
+                Siguiente
+              </button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-          <button
-            className="step__button step__button--back"
-            onClick={() => {
-              if (page === FormTitles.length - 1) {
-                alert('FORM SUBMITTED');
-                console.log(formData);
-              } else {
-                setPage((currPage) => currPage + 1);
-              }
-            }}
-          >
-            {page === FormTitles.length - 1 ? 'Envíar' : 'Siguiente'}
-          </button>
+const Step2 = () => {
+  return (
+    <div className="form-register__body">
+      <div className="step active" id="step-2">
+        <div className="step__header" />
+        <div className="step__body">
+          <p>¿Cuánto te gustaría recaudar?</p>
+          <input
+            type="number"
+            name="objetive"
+            // value={data.objetive}
+            // onChange={(e) => {
+            //   setData({ ...data, objetive: e.target.value });
+            // }}
+            id="objetive"
+            placeholder="USD"
+            className="step__input"
+          />
+          <p className="smalltext">
+            Ten en cuenta que de cada donativo se deducen comisiones por
+            transacción de pago, incluidos cargos por operaciones con tarjetas
+            de crédito y débito.
+          </p>
+          <p>¿Cuál es la fecha límite de recaudación?</p>
+          <input
+            type="date"
+            name="targetdate"
+            // value={data.targetdate}
+            // onChange={(e) => {
+            //   setData({ ...data, targetdate: e.target.value });
+            // }}
+            id="targetdate"
+            placeholder=""
+            className="step__input"
+          />
         </div>
       </div>
     </div>
   );
 };
-
-export default CampaignForm;
+const Step3 = () => {
+  return (
+    <div className="form-register__body">
+      <div className="step active" id="step-3">
+        <div className="step__header" />
+        <div className="step__body">
+          <p>¿Cuál es el titulo de tu campaña?</p>
+          <input
+            type="text"
+            name="title"
+            // value={data.title}
+            // onChange={(e) => {
+            //   setdata({ ...data, title: e.target.value });
+            // }}
+            id="title"
+            placeholder="Título"
+            className="step__input"
+          />
+          <p>¿Porque vas a recaudar fondos? </p>
+          <div className="editor de texto">
+            <textarea />
+          </div>
+          <p>Recuerda agregar una imagen</p>
+          <input
+            type="file"
+            name="img"
+            // value={data.img}
+            // onChange={(e) => {
+            //   setFormData({ ...data, img: e.target.value });
+            // }}
+            id="img"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
