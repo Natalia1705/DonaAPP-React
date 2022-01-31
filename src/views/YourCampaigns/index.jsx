736f8f8828f553e8335e60e748c276d457594eac
Franco <francomelgar4@gmail.com>
2022-01-31 08:44:22 -0500

    Campaings fixed

--- src/views/YourCampaigns/index.jsx
+++ src/views/YourCampaigns/index.jsx
@@ -3,6 +3,7 @@
 /* eslint-disable no-underscore-dangle */
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 import Auth from '../../utils/Auth';
 
 const YourCampaigns = () => {
@@ -16,48 +17,54 @@ const YourCampaigns = () => {
   }, []);
 
   return (
-    <div className="container">
-      <div
-        className="
+    <Container>
+      <div className="container">
+        <div
+          className="
           d-md-flex
           mt-5
           flex-md-row
           align-items-md-center
           justify-content-between
         "
-      >
-        <h4>Tus campañas</h4>
-        <Link to="/campaignform">
-          <button className="btn btn-outline-primary m-2" type="button">
-            + Nueva recaudacion de fondos
-          </button>
-        </Link>
-      </div>
-      <div className="my-5">
-        <div className="row">
-          {campaigns.length
-            ? campaigns.map((e) => (
-                <div className="p-0 col-md-6 col-lg-4" key={e._id}>
-                  <div className="card m-3 shadow bg-body rounded">
-                    <img src={e.img} className="card-img-top" alt={e.title} />
-                    <div className="card-body">
-                      <h5 className="card-title">{e.title}</h5>
-                      <p className="card-text fw-light fs-6">
-                        Fecha de creación: {e.date}
-                      </p>
-                      <hr />
-                      <a href="/" className="btn btn-outline-primary">
-                        Editar
-                      </a>
+        >
+          <h4>Tus campañas</h4>
+          <Link to="/campaignform">
+            <button className="btn btn-outline-primary m-2" type="button">
+              + Nueva recaudacion de fondos
+            </button>
+          </Link>
+        </div>
+        <div className="my-5">
+          <div className="row">
+            {campaigns.length
+              ? campaigns.map((e) => (
+                  <div className="p-0 col-md-6 col-lg-4" key={e._id}>
+                    <div className="card m-3 shadow bg-body rounded">
+                      <img src={e.img} className="card-img-top" alt={e.title} />
+                      <div className="card-body">
+                        <h5 className="card-title">{e.title}</h5>
+                        <p className="card-text fw-light fs-6">
+                          Fecha de creación: {e.date}
+                        </p>
+                        <hr />
+                        <a href="/" className="btn btn-outline-primary">
+                          Editar
+                        </a>
+                      </div>
                     </div>
                   </div>
-                </div>
-              ))
-            : 'Parece que aún no tienes campañas creadas :('}
+                ))
+              : 'Parece que aún no tienes campañas creadas :('}
+          </div>
         </div>
       </div>
-    </div>
+    </Container>
   );
 };
 
 export default YourCampaigns;
+
+const Container = styled.div`
+  margin-top: 150px;
+`;
