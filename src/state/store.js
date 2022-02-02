b1c8ecb7a105feb58746c8b61f54a7825a02a869
torvic <130111@unsaac.edu.pe>
2022-02-02 18:34:51 -0500

    Add payment and slice and use this in store

--- src/state/store.js
+++ src/state/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './auth/authSlice';
 import navBarLoginSlice from './auth/navBarLoginSlice';
+import paymentReducer from './payment/paymentSlice';
 
 export default configureStore({
   reducer: {
     authReducer,
     navBarLoginSlice,
+    paymentReducer,
   },
 });
