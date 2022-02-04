8d8b51d7f24ded20c806bf76c0db4dfbb14b097f
torvic <130111@unsaac.edu.pe>
2022-02-03 19:39:35 -0500

    Add Bearer token to payment request

--- src/api/payment/paymentRequest.js
+++ src/api/payment/paymentRequest.js
@@ -1,36 +1,43 @@
+import Auth from '../../utils/Auth';
+
 const URL_BASE = 'https://fast-shelf-59848.herokuapp.com/api';
 
 export const createCardToken = (card) => {
+  const session = Auth.getSession();
   const query = fetch(`${URL_BASE}/payment/card-token`, {
     method: 'POST',
     mode: 'cors',
     headers: {
       'Content-Type': 'application/json',
+      Authorization: `Bearer ${session?.token}`,
     },
     body: JSON.stringify(card),
   });
   return query;
 };
 
-export const createCustomer = (customer) => {
+export const createCustomer = () => {
+  const session = Auth.getSession();
   const query = fetch(`${URL_BASE}/payment/create-customer`, {
     method: 'POST',
     mode: 'cors',
     headers: {
       'Content-Type': 'application/json',
+      Authorization: `Bearer ${session?.token}`,
     },
-    body: JSON.stringify(customer),
   });
 
   return query;
 };
 
 export const makePayment = (data) => {
+  const session = Auth.getSession();
   const query = fetch(`${URL_BASE}/payment/make-payment`, {
     method: 'POST',
     mode: 'cors',
     headers: {
       'Content-Type': 'application/json',
+      Authorization: `Bearer ${session?.token}`,
     },
     body: JSON.stringify(data),
   });
