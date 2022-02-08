d167d1e35cb993a73b1eca905ddf014a63ac1423
torvic <130111@unsaac.edu.pe>
2022-02-08 12:38:58 -0500

    Add creditCardComponent to Donate page

--- src/components/SummaryCard/index.js
+++ src/components/SummaryCard/index.js
@@ -1,31 +1,43 @@
 import React from 'react';
+import CreditCardComponent from '../../views/Donate/components/CreditCardComponent';
 import './styles.scss';
 
-const SummaryCard = ({ donateAmount, voluntaryInput }) => (
-  <div className="summary">
-    <p className="summary__title">Tu donativo</p>
-    <div className="summary__sum">
-      <p>Tu donativo</p>
-      <p className="sumary__amount">
-        {`${donateAmount === '' ? '0.00' : donateAmount} $`}
-      </p>
-      <p>Aportación para Donapp</p>
-      <p className="sumary__amount">
-        {`${
-          donateAmount === '' ? '0.00' : (donateAmount * voluntaryInput) / 100
-        } $`}
-      </p>
+const SummaryCard = ({ donateAmount, voluntaryInput, id }) => {
+  const totalAmount =
+    Number(donateAmount) + (donateAmount * voluntaryInput) / 100;
+  return (
+    <div>
+      <div className="summary">
+        <p className="summary__title">Tu donativo</p>
+        <div className="summary__sum">
+          <p>Tu donativo</p>
+          <p className="sumary__amount">
+            {`${donateAmount === '' ? '0.00' : donateAmount} $`}
+          </p>
+          <p>Aportación para Donapp</p>
+          <p className="sumary__amount">
+            {`${
+              donateAmount === ''
+                ? '0.00'
+                : (donateAmount * voluntaryInput) / 100
+            } $`}
+          </p>
+        </div>
+        <div className="summary__total">
+          <p>Total a pagar</p>
+          <p className="sumary__amount">
+            {donateAmount === ''
+              ? '0.00'
+              : Number(donateAmount) + (donateAmount * voluntaryInput) / 100}
+            $
+          </p>
+        </div>
+      </div>
+      <div className="summary mt-3">
+        <CreditCardComponent totalAmount={totalAmount * 3965.24} id={id} />
+      </div>
     </div>
-    <div className="summary__total">
-      <p>Total a pagar</p>
-      <p className="sumary__amount">
-        {donateAmount === ''
-          ? '0.00'
-          : Number(donateAmount) + (donateAmount * voluntaryInput) / 100}
-        $
-      </p>
-    </div>
-  </div>
-);
+  );
+};
 
 export default SummaryCard;
