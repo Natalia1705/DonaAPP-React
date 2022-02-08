ff3a71ea153fc1363e515874990f4b73afa7bc4c
torvic <130111@unsaac.edu.pe>
2022-02-08 12:38:04 -0500

    Modify paymetSlice and paymentThunk & add default extrareducer

--- src/thunkAction/paymentThunk.js
+++ src/thunkAction/paymentThunk.js
@@ -18,11 +18,17 @@ export const createCustomerThunk = createAsyncThunk('payment/createCustomer', as
 });
 
 // prettier-ignore
-export const createCardAndCustomerThunk = createAsyncThunk('payment/createCardAndCustomer', async (creditCard) => {
-  const card = await createCardToken(creditCard);
-  const customer = await createCustomer();
-
-  return { card: await card.json(), customer: await customer.json() };
+export const createCardAndCustomerThunk = createAsyncThunk('payment/createCardAndCustomer', async (creditCard, { rejectWithValue }) => {
+  try {
+    const card = await createCardToken(creditCard);
+    const customer = await createCustomer();
+    if (card.status !== 200 || customer.status !== 200) {
+      throw new Error();
+    }
+    return { card: await card.json(), customer: await customer.json() };
+  } catch (error) {
+    return rejectWithValue(error.response.data);
+  }
 });
 
 // prettier-ignore
