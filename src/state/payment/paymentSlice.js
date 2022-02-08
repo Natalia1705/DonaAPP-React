c0eba240a0f294f5df9ab9fc44ce84e43438328e
torvic <130111@unsaac.edu.pe>
2022-02-08 12:38:04 -0500

    Modify paymetSlice and paymentThunk & add default extrareducer

--- src/state/payment/paymentSlice.js
+++ src/state/payment/paymentSlice.js
@@ -1,18 +1,22 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAction } from '@reduxjs/toolkit';
 import {
   createCardAndCustomerThunk,
   makePaymentThunk,
 } from '../../thunkAction/paymentThunk';
 
+export const defaultState = createAction('defaultState');
+
+const initialState = {
+  resPayment: null,
+  resCard: null,
+  loading: false,
+  success: false,
+  error: null,
+};
+
 const paymentSlice = createSlice({
   name: 'payment',
-  initialState: {
-    resPayment: null,
-    resCard: null,
-    loading: false,
-    success: false,
-    error: null,
-  },
+  initialState,
   extraReducers: (builder) => {
     builder.addCase(createCardAndCustomerThunk.fulfilled, (state, action) => {
       state.resCard = action.payload;
@@ -37,6 +41,7 @@ const paymentSlice = createSlice({
       state.loading = false;
       state.error = true;
     });
+    builder.addCase(defaultState, () => initialState);
   },
 });
 
