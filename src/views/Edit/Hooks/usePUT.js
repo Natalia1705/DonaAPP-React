e3d73ee294b4b3ccdef49ed4cda1cdfceb46cdb4
Fabrizzio De Bracamonte <fabrizziodbc@gmail.com>
2022-02-11 11:33:19 -0500

    add sweet alert on comments

--- src/views/Edit/Hooks/usePUT.js
+++ src/views/Edit/Hooks/usePUT.js
@@ -5,13 +5,16 @@ import config from '../../../config';
 const { URL_BASE } = config;
 
 const usePUT = async (id, values) => {
+  const session = Auth.getSession();
   await fetch(`${URL_BASE}/campaigns/${id}`, {
     method: 'PUT',
+    mode: 'cors',
     body: JSON.stringify(values),
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
       'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, DELETE',
+      Authorization: `Bearer ${session?.token}`,
       usertoken: Auth.getSession().token,
     },
   })
