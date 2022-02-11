b8c1d09c9e6def6f70782105081bd9dc689c95d1
Fabrizzio De Bracamonte <fabrizziodbc@gmail.com>
2022-02-11 17:40:32 -0500

    fix share modal css

--- src/config/index.js
+++ src/config/index.js
@@ -2,6 +2,6 @@ import 'dotenv/config';
 
 const config = {
   URL_BASE: process.env.REACT_APP_URL_BASE,
-  NETLIFY_BASE: process.REACT_APP_NETLIFY_BASE,
+  NETLIFY_BASE: process.env.REACT_APP_NETLIFY_BASE,
 };
 export default config;
