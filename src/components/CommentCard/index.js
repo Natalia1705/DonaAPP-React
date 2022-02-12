07e57240aa6123c83d50eebc5f55fe205f50c7c2
Fabrizzio De Bracamonte <fabrizziodbc@gmail.com>
2022-02-11 22:49:02 -0500

    fix comments css

--- src/components/CommentCard/index.js
+++ src/components/CommentCard/index.js
@@ -6,11 +6,9 @@ const CommentCard = ({
   comment = 'Uno a uno todos somos mortales. Juntos, somos eternos.',
 }) => (
   <div className="comment-card">
-    <i className="fas fa-user-circle fa-3x" />
-    <div className="comment-card__container">
-      <p className="comment-card__donator">{name}</p>
-      <p className="comment-card__text">{comment}</p>
-    </div>
+    <i className="fas fa-user-circle fa-3x comment-card__donator__img" />
+    <p className="comment-card__donator">{name} :</p>
+    <p className="comment-card__text">{comment}</p>
   </div>
 );
 
